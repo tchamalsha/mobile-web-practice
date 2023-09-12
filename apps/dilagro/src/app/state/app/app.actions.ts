adfb96a6fb4a57b138d8ebb64ba066577b424eda
tchamalsha <tharu.chamalsha@gmail.com>
2023-09-12 16:42:43 +0530

    add change email

--- apps/dilagro/src/app/state/app/app.actions.ts
+++ apps/dilagro/src/app/state/app/app.actions.ts
@@ -2,3 +2,8 @@ export class ShowLoading {
   public static readonly type = '[App] ShowLoading';
   constructor (public loading:boolean){}
 }
+
+export class ChangeEmail{
+  public static readonly type = '[App] ChangeEmail';
+  constructor (public email:string){}
+}
